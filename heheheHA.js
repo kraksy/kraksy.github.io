a1a705fe1b624fbe6c8dac6db82893d7b665854a
Krek <kraksy8@gmail.com>
2022-06-09 23:27:45 +0200

    edit

--- heheheHA.js
+++ heheheHA.js
@@ -1,5 +1,8 @@
 
-window.addEventListener('load', () =>{
+const mb = document.getElementById("mb");
+const mb2 = document.getElementById("mb2");
+
+mb.addEventListener("click", () =>{
 
     let paint = false;
 
@@ -33,5 +36,30 @@ window.addEventListener('load', () =>{
     canvas.addEventListener("mousedown", start)
     canvas.addEventListener("mouseup", stop)
     canvas.addEventListener("mousemove", draw)
+    
+});
+
+mb2.addEventListener("click", () =>{
+
+    const canvas = document.querySelector('#canvas');
+    const ctx = canvas.getContext('2d')
+
+    function getMousePosition(canvas, event) {
+        let rect = canvas.getBoundingClientRect();
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+        console.log("Coordinate x: " + x, 
+                    "Coordinate y: " + y);
+
+
+                    ctx.strokeRect(x, y, 60, 60);
+    }
+  
+    let canvasElem = document.querySelector("canvas"); 
+      
+    canvasElem.addEventListener("mousedown", function(e)
+    {
+        getMousePosition(canvasElem, e);
+    });
     
 });
